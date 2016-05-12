289774ef673c6a563d6c93e8f20515ceef43cf79
WS061 <hyt@e-tominaga.com>
2016-05-12 11:11:58 +0900

    Added psdwatcher/config.py - operate  configuration

---
+++ psdwatcher/config.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+#coding: utf-8
+
+import os
+
+import cPickle as pickle
+
+DEFAULT_CONTAINER_FILE = str(os.environ.get(
+    'PSDWATCHER_CONTAINER_FILE',
+    os.path.expanduser('~/.psdwatcher')
+))
+
+class FileContainer(object):
+
+    def __init__(self, container_file=DEFAULT_CONTAINER_FILE):
+
+        self.container_file = container_file
+        self.container = self._get_container()
+
+    def add_file(self, file_name):
+
+        file_dir = os.path.dirname(file_name)
+        file_path = os.path.abspath(os.path.join(
+            os.getcwd(),
+            file_name
+        ))
+
+        is_psd = self._is_psd(file_name)
+
+        if is_psd is not True:
+            raise IOError(
+                "'{0}' is not PSD.".format(file_name)
+            )
+        
+        if file_path not in self.container:
+            self.container.append(file_path)
+        else:
+            raise IOError(
+                "'{0}' is duplicated.".format(file_name)
+            )
+
+        self.save()
+
+        return True
+
+    def save(self):
+
+        pickle.dump(self.container, open(self.container_file,"w"))
+
+    def load(self):
+
+        return pickle.load(open(self.container_file,"r"))
+
+    def _is_exist_container_file(self):
+
+        if os.access(self.container_file, os.F_OK) is True:
+            return True
+        else:
+            return False
+
+    def _get_container(self):
+
+        if self._is_exist_container_file() is True:
+            return self.load()
+        else:
+            return []
+
+    def _get_ext(self, file_name):
+
+        return os.path.splitext(file_name)[-1]
+
+    def _is_psd(self, file_name):
+
+        if os.access(file_name, os.F_OK) is True:
+
+            if self._get_ext(file_name) == ".psd":
+                return True
+            else:
+                return False
+
+        else:
+            raise IOError(
+                "'{0}' file doesn't found.".format(file_name)
+            )
+
+
