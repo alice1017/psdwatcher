f7d13cbf98cf02b62b86ca8546ccb9e1714c0ab7
WS061 <hyt@e-tominaga.com>
2016-05-12 11:11:24 +0900

    Added psdwatcher/cli.py - Setting argument parser

---
+++ psdwatcher/cli.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+#coding: utf-8
+
+import sys
+import argparse
+
+from psdwatcher import __version__
+
+parser = argparse.ArgumentParser(
+    prog="psdwatcher",
+    version=__version__,
+    description= \
+        "PSDwatcher - watching psd, and commit automatically."
+)
+
+subparser = parser.add_subparsers(
+    title="commands",
+    dest="command"
+)
+
+parser_add_help = \
+    "PSDwatcher registers a new PSD file to watch list."
+
+parser_add = subparser.add_parser(
+    "add",
+    description=parser_add_help,
+    help=parser_add_help
+)
+
+parser_add.add_argument(
+    "file",
+    action="store",
+    metavar="PSDFILE",
+    help="A PSD file name or path."
+)
+
+parser_run_help = \
+    "PSDwatcher runs the file modification watcher, " \
+    "and automatically exec 'git-add' and 'git-commit'. " \
+
+parser_run = subparser.add_parser(
+    "run",
+    description=parser_run_help,
+    help=parser_run_help
+)
+
+parser_run.add_argument(
+    "--quiet",
+    action="store_true",
+    dest="is_quiet",
+    help="PSDwatcher doesn't print log."
+)
+
+parser_run.add_argument(
+    "--verbose",
+    action="store_true",
+    dest="is_verbose",
+    help="PSDwatcher print verbose log."
+)
+
+parser_list_help = \
+    "PSDwatcher shows the list of all " \
+    "registered PSD file."
+
+parser_list = subparser.add_parser(
+    "list",
+    description=parser_list_help,
+    help=parser_list_help
+)
+
+
+
