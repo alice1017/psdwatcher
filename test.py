2a4f606c75272e63ddd85caa88266883ec9f9a3e
WS061 <hyt@e-tominaga.com>
2016-05-13 10:36:42 +0900

    Updated test runner

--- test.py
+++ test.py
@@ -3,14 +3,12 @@
 
 import os
 
-from unittest import TextTestRunner
-
-try:
-    from unittest import loader
-except ImportError:
-    from unittest2 import loader
+from unittest import loader, TextTestRunner
 
 test_path = os.path.join(os.path.dirname(__file__), 'tests')
 test_loader = loader.TestLoader()
 
-TextTestRunner(verbosity=2).run(test_loader.discover(test_path))
+if __name__ == "__main__":
+
+    runner = TextTestRunner(verbosity=2)
+    runner.run(test_loader.discover(test_path))
