595c89b64f342797798adaf846e001d40d7f08c6
alice <takemehighermore@gmail.com>
2016-05-27 01:33:46 +0900

    [#6]: Fixed pep8 at test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#coding: utf-8
+# coding: utf-8
 
 import os
 import sys
@@ -14,8 +14,7 @@ if __name__ == "__main__":
     runner = TextTestRunner(verbosity=2)
     test_result = runner.run(test_loader.discover(test_path)).wasSuccessful()
 
-    if test_result == False:
+    if test_result is False:
         sys.exit(1)
     else:
         sys.exit(0)
-
